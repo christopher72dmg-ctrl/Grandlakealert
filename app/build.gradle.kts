e07af47f07fb6ef1c38861f9fe60f39e5c6f6468
christopher72dmg-ctrl <christopher72dmg@gmail.com>
2026-09-24 12:46:12 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,13 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-android { namespace = "ca.grandlake.alert"; compileSdk = 35
+android compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}{ namespace = "ca.grandlake.alert"; compileSdk = 35
     defaultConfig { applicationId = "ca.grandlake.alert"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "0.2.0" }
+}kotlin {
+    jvmToolchain(17)
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
