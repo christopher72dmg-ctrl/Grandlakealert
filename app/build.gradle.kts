020c671a9ee57153bef88fad6de3e25b0baafc02
christopher72dmg-ctrl <christopher72dmg@gmail.com>
2026-09-25 15:47:23 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-}                                                                         
+}
+
+android {
     namespace = "ca.grandlake.alert"
     compileSdk = 35
 
