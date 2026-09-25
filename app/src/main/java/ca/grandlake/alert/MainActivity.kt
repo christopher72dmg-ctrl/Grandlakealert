f1fdd5807a68205629501d5643a3a3de3e73f69b
christopher72dmg-ctrl <christopher72dmg@gmail.com>
2026-09-25 12:34:12 -0300

    Update MainActivity.kt

--- app/src/main/java/ca/grandlake/alert/MainActivity.kt
+++ app/src/main/java/ca/grandlake/alert/MainActivity.kt
@@ -9,6 +9,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
